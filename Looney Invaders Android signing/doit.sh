4eb72149729bb2371d0f32d8f62d5ffdddac496e
Pavel Martynov <paul.martynov@gmail.com>
2019-04-17 11:19:17 +0300

    Refactoring (#1)
    * delete auto generated files (obj, bin, .vs, packages, user, bak)
    * adds standart gitignore
    * git structure standardization
    * fix references
    * update packages reinstall
    * move dublicated resources
    * updates Splash screen image and Circles that are used in screen to screen transitions

--- Looney Invaders Android signing/doit.sh
+++
@@ -1,5 +0,0 @@
-rm ./com.init.looney_invaders_old.apk
-rm ./com.init.looney_invaders.apk
-cp ../LooneyInvaders/LooneyInvaders/LooneyInvaders.Droid/bin/Android/Release/com.init.looney_invaders.apk ./com.init.looney_invaders_old.apk 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./tpetrovic_INIT/tpetrovic_INIT.keystore ./com.init.looney_invaders_old.apk tpetrovic_INIT
-~/Library/Developer/Xamarin/android-sdk-macosx/build-tools/27.0.3/zipalign -f -v 4 ./com.init.looney_invaders_old.apk ./com.init.looney_invaders.apk
