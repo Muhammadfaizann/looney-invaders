63e9a1b4902657e88ee37eaadebc6eedc719cac8
Bass <maxoiduss@gmail.com>
2019-04-30 16:06:35 +0300

    Experimental extension to finding out troubles with app42 functionality

---
+++ Sources/LooneyInvaders.Shared/Extensions/App42ScoreBoardExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using com.shephertz.app42.paas.sdk.csharp.game;
+using com.shephertz.app42.paas.sdk.csharp.user;
+using app42ScoreBoardService = com.shephertz.app42.paas.sdk.csharp.game.ScoreBoardService;
+
+namespace LooneyInvaders.Extensions
+{
+    public static class App42ScoreBoardExtensions
+    {
+        public static Game SaveUserScore(this app42ScoreBoardService service, string gameName, string gameUserName, double gameScore, bool isInDebug = false)
+        {
+            Game game = null;
+            User user = null;
+            if (service == null)
+            {
+                if (isInDebug)
+                { Console.WriteLine("SaveUserScore extension error: app42ScoreBoardService is null"); }
+                return game;
+            }
+            try
+            {
+                var userService = com.shephertz.app42.paas.sdk.csharp.App42API.BuildUserService();
+                user = userService?.GetUser(gameUserName);
+            }
+            catch (Exception ex)
+            {
+                var mess = ex.Message;
+                return game;
+            }
+            finally
+            {
+                if (user == null)
+                {
+                    user = new User();
+                    user.SetUserName(gameUserName);
+                }
+                game = service.SaveUserScore(gameName, gameUserName, gameScore);
+            }
+            return game;
+        }
+    }
+}
+
