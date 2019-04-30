cbcda20e073be0df85edbe9c27495cb82b0e9df6
Bass <maxoiduss@gmail.com>
2019-04-30 16:03:59 +0300

    Extension for an CCSprite

---
+++ Sources/LooneyInvaders.Shared/Extensions/CCSpriteExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CocosSharp;
+using LooneyInvaders.Classes;
+using LooneyInvaders.Model;
+
+namespace LooneyInvaders.Extensions
+{
+    public static class CCSpriteExtension
+    {
+        public static void ChangeVisibility(this CCSprite ccSprite, bool isVisible)
+        {
+            if (ccSprite != null)
+            {
+                ccSprite.Visible = isVisible;
+            }
+        }
+
+        public static void CreateIfNeededAndChangeVisibility(this CCSpriteButton ccSprite, bool isVisible,
+            CCLayerColorExt layer,
+            int x, int y,
+            string imageNameUntapped,
+            string imageNameTapped,
+            int zOrder = 100,
+            ButtonType buttonType = ButtonType.Regular,
+            EventHandler onClick = null)
+        {
+            if (ccSprite == null)
+            {
+                if (layer != null)
+                {
+                    ccSprite = layer.AddButton(x, y, imageNameUntapped, imageNameTapped, zOrder, buttonType);
+                    ccSprite.OnClick += onClick;
+                    ccSprite.Visible = isVisible;
+                }
+            }
+            else
+            {
+                ccSprite.Visible = isVisible;
+            }
+        }
+    }
+}
