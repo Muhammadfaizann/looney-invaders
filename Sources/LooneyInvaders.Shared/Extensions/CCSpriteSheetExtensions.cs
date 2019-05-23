f2418f9fe3a4c1ef95b24a03a2a53c5f7e609c10
Bass <maxoiduss@gmail.com>
2019-05-23 16:50:39 +0300

    SpriteSheet async factory method extension as part of previous commit

---
+++ Sources/LooneyInvaders.Shared/Extensions/CCSpriteSheetExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using CocosSharp;
+using LooneyInvaders.Classes;
+
+namespace LooneyInvaders.Extensions
+{
+    public static class CCSpriteSheetExtensions
+    {
+        public static Task<CCSpriteSheet> CCSpriteSheetFactoryMethodAsync(this object obj, object criteria)
+        {
+            Task <CCSpriteSheet> result = null;
+            CCSpriteSheet sheet = null;
+            Type criteriaType = criteria.GetType();
+            if (obj != null && criteriaType == typeof(string))
+            {
+                result = Task.Run(() =>
+                {
+                    try
+                    {
+                        sheet = new CCSpriteSheet(criteria.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"|ON CCSpriteSheetFactoryMethodAsync| Exception: {ex.Message}");
+                    }
+                    return sheet;
+                });
+            }
+            else if (obj == null)
+            {
+                Console.WriteLine("|ON CCSpriteSheetFactoryMethodAsync| Layer is null!");
+            }
+
+            return result;
+        }
+    }
+}
